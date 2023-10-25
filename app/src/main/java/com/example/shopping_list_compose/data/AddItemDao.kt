6013f9b27df0a6e0e694ae130d5eb03df76487b0
oxidante <oxidantered@gmail.com>
2023-10-25 20:04:10 +0500

    work with Dao

--- app/src/main/java/com/example/shopping_list_compose/data/AddItemDao.kt
+++ app/src/main/java/com/example/shopping_list_compose/data/AddItemDao.kt
@@ -5,19 +5,19 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AddItemDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertItem(item: AddItem)
-
     @Delete
     suspend fun deleteItem(item: AddItem)
-
     @Query("SELECT * FROM add_item WHERE listId = :listId")
     fun getAllItemsById(listId: Int): Flow<List<AddItem>>
-
     @Query("SELECT * FROM shop_list_name WHERE Id = :listId")
-    suspend fun getListItemsById(listId: Int): ShoppingListItem
+    suspend fun getListItemById(listId: Int): ShoppingListItem
+    @Update
+    suspend fun insertItem(item: ShoppingListItem)
 }
---
+++ app/src/main/java/com/example/shopping_list_compose/data/NoteDao.kt
@@ -0,0 +1,20 @@
+package com.example.shopping_list_compose.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem(item: NoteItem)
+    @Delete
+    suspend fun deleteItem(item: NoteItem)
+    @Query("SELECT * FROM not_table")
+    fun getAllItems(): Flow<List<NoteItem>>
+    @Query("SELECT * FROM shop_list_name WHERE id = :id")
+    suspend fun getListItemById(id: Int): NoteItem
+}
