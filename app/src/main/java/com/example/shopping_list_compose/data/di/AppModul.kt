c476ddb086c990117383f7c983ace5b65aed6bb9
oxidante <oxidantered@gmail.com>
2023-11-04 19:50:26 +0500

    AppModel

---
+++ app/src/main/java/com/example/shopping_list_compose/data/di/AppModul.kt
@@ -0,0 +1,48 @@
+package com.example.shopping_list_compose.data.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.shopping_list_compose.data.MainDb
+import com.example.shopping_list_compose.data.repository.AddItemRepoImpl
+import com.example.shopping_list_compose.data.repository.AddItemRepository
+import com.example.shopping_list_compose.data.repository.NoteRepoImpl
+import com.example.shopping_list_compose.data.repository.NoteRepository
+import com.example.shopping_list_compose.data.repository.ShoppingListRepoImpl
+import com.example.shopping_list_compose.data.repository.ShoppingListRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModul {
+    @Provides
+    @Singleton
+    fun provideMainDb(app: Application): MainDb {
+        return Room.databaseBuilder(
+            app,
+            MainDb::class.java,
+            "shop_list_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideShopRepo(db: MainDb): ShoppingListRepository {
+        return ShoppingListRepoImpl(db.shoppingListDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAddItemRepo(db: MainDb): AddItemRepository {
+        return AddItemRepoImpl(db.addItemDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepo(db: MainDb): NoteRepository {
+        return NoteRepoImpl(db.noteDao)
+    }
+}
